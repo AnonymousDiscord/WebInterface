d1a60aa61c6215a536ca303d724bbb0f60873c6b
Hauke Weinert <Hauke.Weinert@encoway.de>
2022-02-19 14:13:12 +0100

    added clean-up function to fix errors

--- src/hooks/Stats.tsx
+++ src/hooks/Stats.tsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 import { Stats } from "../structure/JsonTypes";
 import { getStats } from "../utils/Request";
 
-
-export function useStats(): Stats|undefined {
+export function useStats(): Stats | undefined {
   const [stats, setStats] = useState<Stats>()
-  useEffect(() => {getStats().then(setStats)}, [])
+  useEffect(() => {
+    getStats().then(setStats)
+    return setStats as () => void
+  }, [])
   return stats
 }
