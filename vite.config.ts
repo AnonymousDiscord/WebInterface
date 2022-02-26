39792697d6f4adb205fb25491dad92077913d883
Hauke Weinert <Hauke.Weinert@encoway.de>
2022-02-26 19:52:46 +0100

    fixed error

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,17 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    {
+      name: 'Auto Minify',
+      transformIndexHtml: {
+        // This short method reduces the index.html by 24% (from 2.07 KiB to 1.57 KiB)
+        transform: (html: string) => html.replaceAll(/<!--([^]*?)-->|\s?\B\s?/g, "")
+      },
+    }
+  ],
   server: {
     port: 3000,
   },
